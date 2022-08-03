f5993f2fce4051fe64020a5f95611ec7cfe98b67
Mike <mikz@itu.dk>
2022-08-03 10:34:59 +0200

    Add pre-training script for mBERT.

--- src/run_mlm_mbert.py
+++ src/run_mlm_mbert.py
@@ -227,35 +227,24 @@ class DataTrainingArguments:
                 if extension not in ["csv", "json", "txt"]:
                     raise ValueError("`validation_file` should be a csv, a json or a txt file.")
 
-
-### MODIFY THIS
 @dataclass
 class TextDatasetForNextSentencePrediction(Dataset):
-    """
-    This will be superseded by a framework-agnostic approach soon.
-    """
-
     def __init__(
-        self,
-        tokenizer: PreTrainedTokenizer,
-        file_path: str,
-        block_size: int,
-        overwrite_cache=False,
-        short_seq_probability=0.1,
-        nsp_probability=0.5,
-    ):
+            self,
+            tokenizer: PreTrainedTokenizer,
+            file_path: str,
+            block_size: int,
+            overwrite_cache=False,
+            ):
 
         if not os.path.isfile(file_path):
             raise ValueError(f"Input file path {file_path} not found")
 
-        self.short_seq_probability = short_seq_probability
-        self.nsp_probability = nsp_probability
-
         directory, filename = os.path.split(file_path)
         cached_features_file = os.path.join(
-            directory,
-            f"cached_nsp_{tokenizer.__class__.__name__}_{block_size}_{filename}",
-        )
+                directory,
+                f"cached_drp_{tokenizer.__class__.__name__}_{block_size}_{filename}",
+                )
 
         self.tokenizer = tokenizer
 
@@ -263,145 +252,127 @@ class TextDatasetForNextSentencePrediction(Dataset):
         # and the others will use the cache.
         lock_path = cached_features_file + ".lock"
 
-        # Input file format:
-        # (1) One sentence per line. These should ideally be actual sentences, not
-        # entire paragraphs or arbitrary spans of text. (Because we use the
-        # sentence boundaries for the "next sentence prediction" task).
-        # (2) Blank lines between documents. Document boundaries are needed so
-        # that the "next sentence prediction" task doesn't span between documents.
-        #
-        # Example:
-        # I am very happy.
-        # Here is the second sentence.
-        #
-        # A new document.
-
         with FileLock(lock_path):
             if os.path.exists(cached_features_file) and not overwrite_cache:
                 start = time.time()
                 with open(cached_features_file, "rb") as handle:
                     self.examples = pickle.load(handle)
                 logger.info(
-                    f"Loading features from cached file {cached_features_file} [took %.3f s]", time.time() - start
-                )
+                        f"Loading features from cached file {cached_features_file} [took %.3f s]", time.time() - start
+                        )
             else:
                 logger.info(f"Creating features from dataset file at {directory}")
 
-                self.documents = [[]]
+                # We create three data structures to account for the ESCO relation prediction objective
+                self.documents = []
+                self.linked = defaultdict(list)
+                self.contiguous = defaultdict(list)
+
                 with open(file_path, encoding="utf-8") as f:
-                    while True:
-                        line = f.readline()
+                    for line in f:
+                        line = json.loads(line)
                         if not line:
                             break
-                        line = line.strip()
 
-                        # Empty lines are used as document delimiters
-                        if not line and len(self.documents[-1]) != 0:
-                            self.documents.append([])
-                        tokens = tokenizer.tokenize(line)
+                        description = line[list(line.keys())[0]]
+                        tokens = tokenizer.tokenize(description)
                         tokens = tokenizer.convert_tokens_to_ids(tokens)
+
                         if tokens:
-                            self.documents[-1].append(tokens)
+                            key = list(line.keys())[0]
+                            self.documents.append({key: tokens})
+                            self.linked[key].append(tokens)  # linked via esco code (same page)
+                            self.contiguous[key].append(tokens)  # same level 2 major group
 
                 logger.info(f"Creating examples from {len(self.documents)} documents.")
                 self.examples = []
-                for doc_index, document in enumerate(self.documents):
-                    self.create_examples_from_document(document, doc_index, block_size)
+                self.create_examples_from_document(block_size)
 
                 start = time.time()
                 with open(cached_features_file, "wb") as handle:
                     pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
                 logger.info(
-                    f"Saving features into cached file {cached_features_file} [took {time.time() - start:.3f} s]"
-                )
+                        f"Saving features into cached file {cached_features_file} [took {time.time() - start:.3f} s]"
+                        )
 
-    def create_examples_from_document(self, document: List[List[int]], doc_index: int, block_size: int):
-        """Creates examples for a single document."""
-
-        max_num_tokens = block_size - self.tokenizer.num_special_tokens_to_add(pair=True)
-
-        # We *usually* want to fill up the entire sequence since we are padding
-        # to `block_size` anyways, so short sequences are generally wasted
-        # computation. However, we *sometimes*
-        # (i.e., short_seq_prob == 0.1 == 10% of the time) want to use shorter
-        # sequences to minimize the mismatch between pretraining and fine-tuning.
-        # The `target_seq_length` is just a rough target however, whereas
-        # `block_size` is a hard limit.
-        target_seq_length = max_num_tokens
-        if random.random() < self.short_seq_probability:
-            target_seq_length = random.randint(2, max_num_tokens)
-
-        current_chunk = []  # a buffer stored current working segments
-        current_length = 0
+    def create_examples_from_document(self):
+        # Here we make the strong assumption that a combination of two ESCO descriptions is <512 tokens. From our
+        # analysis it seems that the average number of tokens of one description is around 30 tokens.
         i = 0
 
-        while i < len(document):
-            segment = document[i]
-            current_chunk.append(segment)
-            current_length += len(segment)
-            if i == len(document) - 1 or current_length >= target_seq_length:
-                if current_chunk:
-                    # `a_end` is how many segments from `current_chunk` go into the `A`
-                    # (first) sentence.
-                    a_end = 1
-                    if len(current_chunk) >= 2:
-                        a_end = random.randint(1, len(current_chunk) - 1)
+        while i < len(self.documents):
+            segment = self.documents[i]
+            current_code = list(segment.keys())[0]
 
-                    tokens_a = []
-                    for j in range(a_end):
-                        tokens_a.extend(current_chunk[j])
+            tokens_a = []
+            tokens_a.extend(list(segment.values())[0])
 
-                    tokens_b = []
+            tokens_b = []
 
-                    if len(current_chunk) == 1 or random.random() < self.nsp_probability:
-                        is_random_next = True
-                        target_b_length = target_seq_length - len(tokens_a)
+            current_rand_val = random.random()
 
-                        # This should rarely go for more than one iteration for large
-                        # corpora. However, just to be careful, we try to make sure that
-                        # the random document is not the same as the document
-                        # we're processing.
-                        for _ in range(10):
-                            random_document_index = random.randint(0, len(self.documents) - 1)
-                            if random_document_index != doc_index:
-                                break
+            # random
+            if current_rand_val < 0.33:
+                is_random_next = True
+                is_linked_next = False
 
-                        random_document = self.documents[random_document_index]
-                        random_start = random.randint(0, len(random_document) - 1)
-                        for j in range(random_start, len(random_document)):
-                            tokens_b.extend(random_document[j])
-                            if len(tokens_b) >= target_b_length:
-                                break
-                        # We didn't actually use these segments so we "put them back" so
-                        # they don't go to waste.
-                        num_unused_segments = len(current_chunk) - a_end
-                        i -= num_unused_segments
-                    # Actual next
-                    else:
-                        is_random_next = False
-                        for j in range(a_end, len(current_chunk)):
-                            tokens_b.extend(current_chunk[j])
+                # This should rarely go for more than one iteration for large
+                # corpora. However, just to be careful, we try to make sure that
+                # the random document is not the same as the document
+                # we're processing.
+                for _ in range(10):
+                    random_document_index = random.randint(0, len(self.documents) - 1)
+                    if self.documents[random_document_index] != segment:
+                        break
 
-                    if not (len(tokens_a) >= 1):
-                        raise ValueError(f"Length of sequence a is {len(tokens_a)} which must be no less than 1")
-                    if not (len(tokens_b) >= 1):
-                        raise ValueError(f"Length of sequence b is {len(tokens_b)} which must be no less than 1")
+                random_document_index = random.randint(0, len(self.documents) - 1)
+                random_document = self.documents[random_document_index]
+                random_document = list(random_document.values())[0]
+                tokens_b.extend(random_document)
 
-                    # add special tokens
-                    input_ids = self.tokenizer.build_inputs_with_special_tokens(tokens_a, tokens_b)
-                    # add token type ids, 0 for sentence a, 1 for sentence b
-                    token_type_ids = self.tokenizer.create_token_type_ids_from_sequences(tokens_a, tokens_b)
+            # linked
+            elif 0.33 <= current_rand_val < 0.66:
+                is_linked_next = True
+                is_random_next = False
 
-                    example = {
-                        "input_ids": torch.tensor(input_ids, dtype=torch.long),
-                        "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
-                        "next_sentence_label": torch.tensor(1 if is_random_next else 0, dtype=torch.long),
+                random_document_index = random.randint(0, len(self.linked[current_code]) - 1)
+                linked_document = self.linked[current_code][random_document_index]
+                tokens_b.extend(linked_document)
+
+            # contiguous
+            else:
+                is_random_next = False
+                is_linked_next = False
+                random_document_index = random.randint(0, len(self.contiguous[current_code[:2]]) - 1)
+                contiguous_document = self.contiguous[current_code[:2]][random_document_index]
+                tokens_b.extend(contiguous_document)
+
+            # check if there is even a description
+            if not (len(tokens_a) >= 1):
+                raise ValueError(f"Length of sequence a is {len(tokens_a)} which must be no less than 1")
+            if not (len(tokens_b) >= 1):
+                raise ValueError(f"Length of sequence b is {len(tokens_b)} which must be no less than 1")
+
+            # add special tokens
+            input_ids = self.tokenizer.build_inputs_with_special_tokens(tokens_a, tokens_b)
+            # add token type ids, 0 for sentence a, 1 for sentence b
+            token_type_ids = self.tokenizer.create_token_type_ids_from_sequences(tokens_a, tokens_b)
+
+            # add labels for erp objective
+            if is_random_next:
+                label = 0
+            elif is_linked_next:
+                label = 1
+            else:
+                label = 2
+
+            example = {
+                    "input_ids": torch.tensor(input_ids, dtype=torch.long),
+                    "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+                    "drp_label": torch.tensor(label, dtype=torch.long),
                     }
 
-                    self.examples.append(example)
-
-                current_chunk = []
-                current_length = 0
+            self.examples.append(example)
 
             i += 1
 
