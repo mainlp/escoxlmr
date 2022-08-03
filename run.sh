414046711a21fa5681db638178fe734814977341
Mike <mikz@itu.dk>
2022-08-03 10:32:39 +0200

    Add bash script to train model.

--- run.sh
+++ run.sh
@@ -1,19 +1,11 @@
-#python3 src/run_language_modeling.py --train_data_file resources/ai_domainlevel.txt \
-#                                     --line_by_line \
-#                                     --output_dir distilbert-test \
-#                                     --model_type distilbert \
-#                                     --tokenizer_name distilbert-base-cased \
-#                                     --per_gpu_train_batch_size 16 \
-#                                     --gradient_accumulation_steps 128  \
-#                                     --model_name_or_path distilbert-base-cased \
-#                                     --do_train \
-#                                     --max_steps 12500  \
-#                                     --learning_rate 0.0005 \
-#                                     --mlm
-
-python3 src/run_language_modeling.py --output_dir=distilbert-test \
-                                     --model_type=distilbert \
-                                     --model_name_or_path=distilbert-base-cased \
-                                     --do_train \
-                                     --train_data_file=resources/ai_domainlevel.txt \
-                                     --mlm
+python3 src/run_mlm_xlmr.py --train_file resources/dummy.txt \
+                            --line_by_line \
+                            --output_dir xlm-test \
+                            --model_type xlm-roberta-large \
+                            --tokenizer_name xlm-roberta-large \
+                            --per_device_train_batch_size 64 \
+                            --gradient_accumulation_steps 128  \
+                            --model_name_or_path xlm-roberta-large \
+                            --do_train \
+                            --max_steps 12500  \
+                            --learning_rate 0.0005
